1c805640472e1ce3806b2ce6fa06de4e768bdade
aorysan <aryoadiputro@gmail.com>
2023-09-06 08:56:40 +0700

    ppp

--- p.java
+++ p.java
@@ -1,5 +1,5 @@
 public class p {
-    public static void main(String[] args {
-        System.out.print("ppp");
+    public static void main(String[] args) {
+        System.out.print("hello from Github");
     }
 }
